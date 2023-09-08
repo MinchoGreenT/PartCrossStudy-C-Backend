3b02c59abf0f3b1db06d7fcec1bf4b900b171252
MinchoGreenT <dldmstjq99@gmail.com>
2023-09-08 20:38:26 +0900

    initial commit

---
+++ src/main/java/com/example/teamc/TeamcApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamc/TeamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
